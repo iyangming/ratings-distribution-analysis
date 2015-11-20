00b0b7223b93b0a0389fccdbb075a3839838bba5
Max Harper <maxharp3r@gmail.com>
2015-11-20 15:23:44 -0600

    create datafiles

--- generate_data.py
+++ generate_data.py
@@ -5,18 +5,25 @@ import csv
 # connect
 db = MySQLdb.connect(host="127.0.0.1", user="web", passwd="atth1132", db="ML3_mirror")
 
-def raw_ml3_ml4():
+def raw(fn, query):
     with db as cursor:
-        cursor.execute("SELECT * FROM ML3_mirror.user_rating_pairs where source!='ML4' limit 10")
+        cursor.execute(query)
 
         # get the number of rows in the resultset
         numrows = int(cursor.rowcount)
-        with open("output.csv", "wb") as f:
-            writer = csv.writer(f,delimiter=',', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+        with open("out/%s" % fn, "wb") as f:
+            writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
             writer.writerow(['userId', 'movieId', 'rating', 'tstamp', 'source'])
             for x in range(0,numrows):
                 row = cursor.fetchone()
                 writer.writerow([row[0], row[1], row[2], row[3], row[4]])
 
+def raw_ml3():
+    raw("raw_ml3.csv", "SELECT * FROM ML3_mirror.user_rating_pairs where source!='ML4'")
+
+def raw_ml4():
+    raw("raw_ml4.csv", "SELECT * FROM ML3_mirror.user_rating_pairs where source='ML4'")
+
 if __name__ == "__main__":
-    raw_ml3_ml4()
+    raw_ml3()
+    raw_ml4()
