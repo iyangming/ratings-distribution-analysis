11b6514787fd99fcc328a1a685a6b754ef7c45fe
Max Harper <maxharp3r@gmail.com>
2015-11-20 15:17:03 -0600

    add .gitignore and requirements; factor query into a function

--- connecttomysql.py
+++ connecttomysql.py
@@ -3,22 +3,20 @@ import MySQLdb
 import csv
 
 # connect
-db = MySQLdb.connect(host="localhost", user="web", passwd="atth1132",
-db="ML3_mirror")
+db = MySQLdb.connect(host="127.0.0.1", user="web", passwd="atth1132", db="ML3_mirror")
 
-cursor = db.cursor()
+def raw_ml3_ml4():
+    with db as cursor:
+        cursor.execute("SELECT * FROM ML3_mirror.user_rating_pairs where source!='ML4' limit 10")
 
-# execute SQL select statement
-cursor.execute("SELECT * FROM ML3_mirror.user_rating_pairs where source!='ML4'")
+        # get the number of rows in the resultset
+        numrows = int(cursor.rowcount)
+        with open("output.csv", "wb") as f:
+            writer = csv.writer(f,delimiter=',', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+            writer.writerow(['userId', 'movieId', 'rating', 'tstamp', 'source'])
+            for x in range(0,numrows):
+                row = cursor.fetchone()
+                writer.writerow([row[0], row[1], row[2], row[3], row[4]])
 
-# commit your changes
-db.commit()
-
-# get the number of rows in the resultset
-numrows = int(cursor.rowcount)
-with open("output.csv", "wb") as f:
-    writer = csv.writer(f,delimiter=',', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
-    writer.writerow(['userId', 'movieId', 'rating', 'tstamp', 'source'])
-    for x in range(0,numrows):
-   		row = cursor.fetchone()
-   		writer.writerow([row[0], row[1], row[2], row[3], row[4]])
+if __name__ == "__main__":
+    raw_ml3_ml4()
